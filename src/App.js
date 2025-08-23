bd12eddc8a9771b91e9d504989d8b7db3e7b2e9b
Kawser420 <muhammadkawser202@gmail.com>
2025-08-23 19:54:23 +0600

    replace old code and make new tech

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
-import './App.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Main from './Layout/Main';
-import Home from './components/Home/Home';
-import Products from './components/Products/Products';
-import About from './components/About/About';
-import Friends from './components/Friends/Friends';
-import FriendDetails from './components/FriendDetails/FriendDetails';
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "./layout/Main";
+import Home from "./components/Home/Home";
+import Products from "./components/Products/Products";
+import About from "./components/About/About";
+import Friends from "./components/Friends/Friends";
+import FriendDetails from "./components/FriendDetails/FriendDetails";
 
 function App() {
-    const router = createBrowserRouter([
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        { path: "/", element: <Home></Home> },
+        { path: "/home", element: <Home></Home> },
+        { path: "/products", element: <Products></Products> },
         {
-            path: '/',
-            element: <Main></Main>,
-            children: [
-                { path: '/', element: <Home></Home> },
-                { path: '/home', element: <Home></Home> },
-                { path: '/products', element: <Products></Products> },
-                {
-                    path: '/friends',
-                    loader: async () => {
-                        return fetch('https://jsonplaceholder.typicode.com/users')
-                    },
-                    element: <Friends></Friends>,
-                },
-                {
-                    path: '/friend/:friendId',
-                    loader: async ({ params }) => {
-                        console.log(params)
-                        return fetch(`https://jsonplaceholder.typicode.com/users/${params.friendId}`)
-                    },
-                    element: <FriendDetails></FriendDetails>
-                }
-            ],
+          path: "/friends",
+          loader: async () => {
+            return fetch("https://jsonplaceholder.typicode.com/users");
+          },
+          element: <Friends></Friends>,
         },
-        { path: 'about', element: <About></About> },
-        { path: '*', element: <div>This Route Not Found!!!</div> }
-    ]);
-    return (
-        <div className="App">
-            <RouterProvider router={router}></RouterProvider>
-        </div>
-    );
-};
+        {
+          path: "/friend/:friendId",
+          loader: async ({ params }) => {
+            console.log(params);
+            return fetch(
+              `https://jsonplaceholder.typicode.com/users/${params.friendId}`
+            );
+          },
+          element: <FriendDetails></FriendDetails>,
+        },
+      ],
+    },
+    { path: "about", element: <About></About> },
+    { path: "*", element: <div>This Route Not Found!!!</div> },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <nav>
-                <Link to={'/'}>Home</Link>
-                <Link to={'home'}>Home</Link>
-                <Link to={'products'}>Products</Link>
-                <Link to={'friends'}>Friends</Link>
-                <Link to={'about'}>About</Link>
-            </nav>
-            <h2>Common Pages</h2>
-        </div>
-    );
+  return (
+    <div className="header">
+      <nav>
+        <Link to={"/"}>Login</Link>
+        <Link to={"home"}>Home</Link>
+        <Link to={"products"}>Products</Link>
+        <Link to={"friends"}>Friends</Link>
+        <Link to={"about"}>About</Link>
+      </nav>
+      <h2>Common Pages</h2>
+    </div>
+  );
 };
 
 export default Header;
